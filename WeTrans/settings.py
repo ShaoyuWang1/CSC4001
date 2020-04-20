14971752b767f9c49f34bd96bd5361faf33b810c
Jacket <117010420@link.cuhk.edu.cn>
2020-04-20 21:10:22 +0800

    Select sqlite3 as project database (a local database)

--- WeTrans/settings.py
+++ WeTrans/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Backend',
     'xadmin',
     'crispy_forms',
 ]
@@ -80,12 +81,11 @@ WSGI_APPLICATION = 'WeTrans.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'WeTrans',
-        'USER': 'temp',
-        'PASSWORD': '123456',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
